0e791ef8fa0c9271f3646ce1d1539374b527ac32
Michael Ovakimian <mov.express17@gmail.com>
2023-12-11 15:08:12 +0300

    Final commit

--- answer_extractor.py
+++ answer_extractor.py
@@ -1,65 +1,45 @@
-from vector_db.weaviate_db import WeaviateDB
+from legal_answer_extraction.vector_db.weaviate_db import WeaviateDB
 from sentence_transformers import SentenceTransformer
-from article_db.pandas_article_db import PandasArticleDB
+from legal_answer_extraction.article_db.pandas_article_db import PandasArticleDB
+from legal_answer_extraction.qa_model.openai_qa import OpenAIModel
 from transformers import pipeline
 
 
 class AnswerExtractor:
+
     def __init__(
             self,
-            vector_db: WeaviateDB,
-            article_db: PandasArticleDB,
-            qa_model: SentenceTransformer,
-            sequence_encoder
+            vector_db,
+            article_db,
+            qa_model,
+            sequence_encoder,
     ):
         self.vector_db = vector_db
         self.article_db = article_db
         self.qa_model = qa_model
+        self.gen_model = OpenAIModel('sk-uJPlEJyqRg8jeYD1rSRxzV0DyXHzQtNb', 'https://api.proxyapi.ru/openai/v1')
         self.sequence_encoder = sequence_encoder
+        self.certainty_thresh = 0.7
 
     def find(self, query):
         enc_query = self.sequence_encoder.encode(query).tolist()
         vdb_response = self.vector_db.find(enc_query)
-        contexts = [item['article_text'] for item in vdb_response]
 
         contexts = [item['article_text'] for item in vdb_response]
         ids = [item['_additional']['id'] for item in vdb_response]
-
-        article_table = self.article_db.get_table('labor_code')
 
         results = []
         for ctx_id, ctx in zip(ids, contexts):
             results.append((ctx_id, self.qa_model(question=query, context=ctx)))
 
-        final_result = max(results, key=lambda x: x[1]['score'])
+        final_result = list(filter(lambda x: x[1]['score'] >= self.certainty_thresh, results))
+
+        prospect_articles = '\n'.join([text['article_name'] + '\n' + text['article_text'] for text in final_result])
+        text_response = self.gen_model(query, prospect_articles)
+
         response = {
-            'part': article_table.loc[final_result[0], 'part'],
-            'section': article_table.loc[final_result[0], 'section'],
-            'chapter': article_table.loc[final_result[0], 'chapter'],
-            'article': article_table.loc[final_result[0], 'article'],
-            'article_text': article_table.loc[final_result[0], 'text'],
-            'start': final_result[1]['start'],
-            'end': final_result[1]['end'],
+            'article_text': text_response
         }
-
-        return response
-
-
-class OpenAIAnswerExtractor:
-    def __init__(
-            self,
-            qa_model,
-            vector_db: WeaviateDB = None,
-            article_db: PandasArticleDB = None,
-            sequence_encoder=None,
-    ):
-        self.vector_db = vector_db
-        self.article_db = article_db
-        self.qa_model = qa_model
-        self.sequence_encoder = sequence_encoder
-
-    def find(self, query, context=None):
-        response = self.qa_model(query)
 
         return response
 
